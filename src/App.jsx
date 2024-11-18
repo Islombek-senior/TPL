f880683eb036c9caf83b16e79b12ce3f6c023ea1
Islombek-senior <islombeknomonov739@gmail.com>
2024-11-18 11:20:56 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
           setProtect,
         }}
       >
-        {/* lkdljfnsdflj */}
+        Second Repository
         <div style={{ background: "#e9e8e840" }}>
           <Router>
             <Header />
